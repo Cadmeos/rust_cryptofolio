76e875dfaa32c7572b7739ea50468cc6e04a8157
Lanky <80473426+Cadmeos@users.noreply.github.com>
2022-01-27 01:05:42 -0300

    Add files via upload

---
+++ rust_folio/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Who knows when?");
+}
---
+++ rust_folio/target/debug/deps/rust_folio-754e3e5e5e860e47.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/pedro/Desktop/Scripts/rust_cryptofolio/rust_folio/target/debug/deps/rust_folio-754e3e5e5e860e47: src/main.rs
+
+/mnt/c/Users/pedro/Desktop/Scripts/rust_cryptofolio/rust_folio/target/debug/deps/rust_folio-754e3e5e5e860e47.d: src/main.rs
+
+src/main.rs:
